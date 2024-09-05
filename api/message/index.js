f733dc76ea63acdfe8d06b08817147c88c5507fe
Ernest Thomas <ernest@ecdigital.com.au>
2024-09-05 12:09:14 +1000

    latest

--- api/message/index.js
+++ api/message/index.js
@@ -1,21 +1,16 @@
 module.exports = async function (context, req) {
-    // context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: JSON.stringify(responseMessage)
-    // };
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: JSON.stringify(responseMessage)
+    };
 
-    context.log(`Http function processed request for url "${request.url}"`);
-
-    const name = request.query.get('name') || await request.text() || 'world';
-
-    return { body: `Hello, ${name}!` };
 }
 
 
